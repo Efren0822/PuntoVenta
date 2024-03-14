3f001c1b7700e51df4a95f963f2d664597967538
Efren <practicas038@gmail.com>
2024-03-14 14:33:03 -0600

    Version 2.1

--- PuntoVenta/Models/ApplicationDbContext.cs
+++ PuntoVenta/Models/ApplicationDbContext.cs
@@ -13,13 +13,34 @@ namespace PuntoVenta.Models
 
         public DbSet<User> Registros { get; set; }
 
+        public DbSet<Products> Productos { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<SubCategorias> SubCategorias { get; set; }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Usuario>().ToTable("UsuUsuario");
+            modelBuilder.Entity<Products>().ToTable("ProProducto")
+                .HasKey(b => b.IdPro);
+            modelBuilder.Entity<Products>()
+        .HasOne(p => p.Categoria)
+        .WithMany()
+        .HasForeignKey(p => p.idProCatCategoria); // Asegúrate de que la propiedad se llama 'IdCat' y no 'CategoriaIdCat'
+
+            modelBuilder.Entity<Products>()
+                .HasOne(p => p.SubCategoria)
+                .WithMany()
+                .HasForeignKey(p => p.idProCatSubCategoria);
+
+            modelBuilder.Entity<Categorias>().ToTable("ProCatCategoria")
+                .HasKey(c => c.IdCat);
+            modelBuilder.Entity<SubCategorias>().ToTable("ProCatSubCategoria")
+                .HasKey(sc => sc.IdSubCat);
 
         }
 
     }
-    }
+}
